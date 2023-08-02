8a02a6854bb3719ff35caa0eaa78d2c8d113f014
UMAR <apple@APPLEs-MacBook-Pro.local>
2023-08-02 14:44:28 +0500

    Testing FYP Project

---
+++ file/receiverReg.php
@@ -0,0 +1,53 @@
+<?php
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if (isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+$PublicIP = get_client_ip();
+// $PublicIP = '59.103.97.43';
+
+
+if(isset($_POST['rregister'])){
+	require 'connection.php';
+	$rname=$_POST['rname'];
+	$remail=$_POST['remail'];
+	$rpassword=$_POST['rpassword'];
+	$rphone=$_POST['rphone'];
+	$rcity=$_POST['rcity'];
+	$rbg=$_POST['rbg'];
+	$ip = $PublicIP;
+	$check_email = mysqli_query($conn, "SELECT remail FROM receivers where remail = '$remail' ");
+	if(mysqli_num_rows($check_email) > 0){
+    $error= 'Email Already exists. Please try another Email.';
+    header( "location:../register.php?error=".$error );
+}else{
+	$sql = "INSERT INTO receivers (rname, remail, rpassword, rphone, rcity, rbg,ip)
+	VALUES ('$rname','$remail', '$rpassword', '$rphone', '$rcity', '$rbg','$ip')";
+	if ($conn->query($sql) === TRUE) {
+		$msg = "You have successfully registered. Please, login to continue.";
+		header( "location:../login.php?msg=".$msg);
+	} else {
+		$error = "Error: " . $sql . "<br>" . $conn->error;
+        header( "location:../register.php?error=".$error );
+	}
+	$conn->close();
+}
+}
+?>
